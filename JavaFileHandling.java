ddc946dd5ee492ff7b579712c7e9667d3f0c7399
Tomas Frohawk <tom@email.com>
2018-05-01 22:51:08 +0100

    added some comments

--- JavaFileHandling.java
+++ JavaFileHandling.java
@@ -5,16 +5,22 @@ class JavaFileHandling {
 	public static void main(String[] args) {
 			
 		try {
+			// open file using BufferedReader
 			BufferedReader br = new BufferedReader(new FileReader("basicTxtFile.txt"));
 			
 			String line = br.readLine();
 			
+			// loop through lines in file, as long as line is not null
 			while (line != null) {
+				// print line out to console
 				System.out.println(line);
+				// read next line of file
 				line = br.readLine();
 			}
+			
 		}
 		catch (Exception e) {
+			// an error has occurred, so print out the following message to the console
 			System.out.println("An error has occurred");
 		}
 		
