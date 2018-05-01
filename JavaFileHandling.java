7585119498f71cee51f964ac15f7db2efdce3455
Tomas Frohawk <tom@email.com>
2018-05-01 22:46:51 +0100

    minor changes

--- JavaFileHandling.java
+++ JavaFileHandling.java
@@ -1,7 +1,22 @@
+import java.io.*;
+
 class JavaFileHandling {
 
 	public static void main(String[] args) {
 		
+		
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+			
+			String line = br.readLine();
+			
+			while (line != null) {
+				System.out.println(line);
+			}
+		}
+		catch (Exception e) {
+			System.out.println("An error has occured");
+		}
 	}
 	
 }
